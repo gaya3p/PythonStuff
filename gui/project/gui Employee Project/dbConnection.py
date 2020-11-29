791c9fdc6253317baff9d35417ae1610ab70bf85
gayatri-p <gaya3p13@gmail.com>
2020-11-29 14:26:08 +0530

    cleanup

--- gui/project/gui Employee Project/dbConnection.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector as m1
-import time
-def returnConnection():
-        mycon = m1.connect(host="127.0.0.1",user="root",password='alohomora',database='company')
-        return mycon
-
-
-	
--- gui/project/gui Employee Project/eee.py
+++
@@ -1,2 +0,0 @@
-import subprocess as sp
-sp.Popen(['notepad.exe','hello.txt'])
